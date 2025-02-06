9ac23164d72c3bfdab47aae44b31221036720e15
Lese <lshernandez@unimayor.edu.co>
2025-02-06 00:23:57 -0500

    fetch comics of service

--- src/app/modules/comics/components/dashboard/dashboard.component.ts
+++ src/app/modules/comics/components/dashboard/dashboard.component.ts
@@ -1,23 +1,45 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
+import { ComicsModule } from '../../comics.module';
+import { ComicsService } from '../../services/comics.service';
+import { CardComicComponent } from "../card-comic/card-comic.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ComicsModule, CardComicComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
 
-  constructor(private router: Router) {
+  values: any[] = []
+
+  constructor(private router: Router, private readonly comicService:ComicsService) {
     if (!this.validateToken()) {
       this.router.navigate(["auth", "login"])
     }
+    this.fetchComics();
+  }
+
+  fetchComics():void {
+    this.comicService.fetchAllComics().subscribe({
+      next:(response: any) => {
+        this.values = response.data.results;
+        console.log(response.data.results)
+      }
+    })
+  }
+
+  generateUrlImage(comic: any):string | undefined {
+    if (comic.images.length > 0) {
+      return `${comic.images[0].path}.${comic.images[0].extension}`;
+    }
+    
+    return undefined;
   }
 
   validateToken():boolean {
-    return false;
+    return true;
   }
 }
